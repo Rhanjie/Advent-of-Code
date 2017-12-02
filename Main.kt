f75dc884c5aaaad0a7c9e7239effbd069881b37a
RhAnjiE <irhanjie@gmail.com>
2017-12-02 23:05:33 +0100

    Day 2 - First Part

--- Main.kt
+++ Main.kt
@@ -5,28 +5,29 @@ fun main(args: Array<String>){
     var sum = 0
 
     val inputStream: InputStream = File("res/input.txt").inputStream()
-    val inputString = inputStream.bufferedReader().use { it.readText() }
+    val lineList = mutableListOf<String>()
 
-    println(inputString)
-
-
-    var secondPosition: Int
-    var iterator = 0
-
-    for(i in inputString.indices){
-        if(inputString.length/2 + i <= inputString.length-1)
-            secondPosition = inputString.length/2 + i
-
-        else secondPosition = iterator++
-
-
-        println("Iterator: $i ... ${inputString.length/2 + i} and $secondPosition")
-
-        if (inputString[i] == inputString[secondPosition]){
-            sum += (inputString[i].toString()).toInt()
-
-            println(" ++ Added sum: $sum (${inputString[i]} and ${inputString[secondPosition]})")
+    (inputStream.bufferedReader()).useLines{
+        lines -> lines.forEach{
+            lineList.add(it)
         }
+    }
+
+    lineList.forEach{
+        var small = 10000
+        var big = 0
+
+        it.split(" , ").forEach {
+            if(it.toInt() < small)
+                small = it.toInt()
+
+            if(it.toInt() > big)
+                big = it.toInt()
+        }
+
+        sum += (big - small)
+
+        println("Difference: ${big - small} : $big - $small")
     }
 
     println("\nEnd! Sum is: $sum")
